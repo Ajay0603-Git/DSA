0afd7629fd0f1a200caca94523783d5905d8dc07
Ajay0603-Git <ajaygithub0603@gmail.com>
2025-10-15 06:59:01 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BubbleSort {
+    public static void main (String s[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int A[]=new int[5];
+        for(int i=0;i<A.length;i++)
+        {
+            A[i]=sc.nextInt();
+        }
+
+        for(int k=0;k<A.length;k++)
+        {
+            for(int j=k+1;j<A.length-1;j++)
+            {
+                if(A[k]>A[j])
+                {
+                    int temp=A[k];
+                    A[k]=A[j];
+                    A[j]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array is:")
+        for(int i=0;i<A.length;i++)
+        {
+            System.out.print(A[i]+"\t");
+        }
+    }
+    
+}
